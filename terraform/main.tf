b891d8f140a57f8784126201a156aaf0e3471197
sish8075 <simon.shadman@gmail.com>
2021-11-07 20:13:29 +0100

    bigquery module

--- terraform/main.tf
+++ terraform/main.tf
@@ -11,3 +11,13 @@ module "foundation" {
   source     = "./modules/fourkeys-foundation"
   project_id = var.project_id
 }
+
+module "bigquery" {
+  source                         = "./modules/fourkeys-bigquery"
+  project_id                     = var.project_id
+  bigquery_region                = var.bigquery_region
+  fourkeys_service_account_email = module.foundation.fourkeys_service_account_email
+  depends_on = [
+    module.foundation
+  ]
+}
